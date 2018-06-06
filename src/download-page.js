86054f3a44b2a0312f35b55ed2f139a4fa5509e0
BLACKDRAGON\user <phapear@outlook.com>
2018-06-06 12:13:38 +0700

    RENTSPREE

--- src/download-page.js
+++ src/download-page.js
@@ -1,29 +1,53 @@
 import React from "react"
 import { Button } from "react-bootstrap"
+import { Navbar} from "react-bootstrap"
+import { Nav} from "react-bootstrap"
+import { NavItem} from "react-bootstrap"
+import { NavDropdown} from "react-bootstrap"
+import { MenuItem} from "react-bootstrap"
+
+import { Glyphicon } from "react-bootstrap"
 import logo from './logo.svg'
 import "./App.scss";
 
 export default class DownloadPage extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <Button>Default</Button>
-          <Button bsStyle="primary">Primary</Button>
-          <Button bsStyle="success">Success</Button>
-          <Button bsStyle="info">Info</Button>
-          <Button bsStyle="warning">Warning</Button>
-          <Button bsStyle="danger">Danger</Button>
-          <Button bsStyle="link">Link</Button>
+<div className="Rents">
+    <div className="RentS-Header">
+        <div className="Inline">
+          <img src={"https://go.crmls.org/wp-content/uploads/2017/06/logo-RentSpree-CRMLS.png"} className="RentS-logo" alt="logo-RentSpre"/>
+        </div>  
+        <div className="Inline">
+          <div className="RentS-title">
+              <h2 ><div className="title-bold">rent</div><div className="title-normal" >spree</div></h2>    
+          </div> 
         </div>
-      </div>
+    </div>
+         
+<Navbar>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#home">React-Bootstrap</a>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Nav>
+    <NavItem eventKey={1} href="#">
+      Link
+    </NavItem>
+    <NavItem eventKey={2} href="#">
+      Link
+    </NavItem>
+    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+      <MenuItem eventKey={3.1}>Action</MenuItem>
+      <MenuItem eventKey={3.2}>Another action</MenuItem>
+      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.4}>Separated link</MenuItem>
+    </NavDropdown>
+  </Nav>
+</Navbar>
+</div>      
     )
   }
 }
